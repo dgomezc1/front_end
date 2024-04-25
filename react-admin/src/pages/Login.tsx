7c539176c7df8ae185fb5b882e1c5a6d3f882c00
Miguel Zapata <miguel@fincrimedynamics.com>
2024-04-24 23:25:28 -0500

    make compatible with google auth

--- react-admin/src/pages/Login.tsx
+++ react-admin/src/pages/Login.tsx
@@ -1,50 +1,69 @@
-import React, {SyntheticEvent, useState} from 'react';
-import '../Login.css';
-import axios from 'axios';
-import {Redirect} from "react-router-dom";
+import React, { SyntheticEvent, useEffect, useState } from "react";
+import "../Login.css";
+import axios from "axios";
+import { Redirect } from "react-router-dom";
+import { Button } from "@material-ui/core";
 
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [redirect, setRedirect] = useState(false);
+  //   const [email, setEmail] = useState("");
+  //   const [password, setPassword] = useState("");
+  //   const [redirect, setRedirect] = useState(false);
 
-    const submit = async (e: SyntheticEvent) => {
-        e.preventDefault();
+  //   const submit = async (e: SyntheticEvent) => {
+  //     e.preventDefault();
 
-        await axios.post('login', {
-            email,
-            password
-        });
+  //     await axios.post("login", {
+  //       email,
+  //       password,
+  //     });
 
-        setRedirect(true);
+  //     setRedirect(true);
+  //   };
+
+  const [token, setToken] = useState("");
+
+  //   if (redirect) {
+  //     return <Redirect to={"/"} />;
+  //   }
+
+  useEffect(() => {
+    if (token) {
+      localStorage.setItem("access_token", token);
     }
+  }, [token]);
 
-    if (redirect) {
-        return <Redirect to={'/'}/>;
-    }
+  return (
+    <div>
+      <Button
+        onClick={() => {
+          window.location.href = "http://localhost:8080/auth/google";
+        }}
+        color="primary"
+      >
+        Log with google
+      </Button>
+    </div>
+    // <main className="form-signin">
+    //     <form onSubmit={submit}>
+    //         <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
 
-    return (
-        <main className="form-signin">
-            <form onSubmit={submit}>
-                <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+    //         <div className="form-floating">
+    //             <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"
+    //                    onChange={e => setEmail(e.target.value)}
+    //             />
+    //             <label htmlFor="floatingInput">Email address</label>
+    //         </div>
+    //         <div className="form-floating">
+    //             <input type="password" className="form-control" id="floatingPassword" placeholder="Password"
+    //                    onChange={e => setPassword(e.target.value)}
+    //             />
+    //             <label htmlFor="floatingPassword">Password</label>
+    //         </div>
 
-                <div className="form-floating">
-                    <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"
-                           onChange={e => setEmail(e.target.value)}
-                    />
-                    <label htmlFor="floatingInput">Email address</label>
-                </div>
-                <div className="form-floating">
-                    <input type="password" className="form-control" id="floatingPassword" placeholder="Password"
-                           onChange={e => setPassword(e.target.value)}
-                    />
-                    <label htmlFor="floatingPassword">Password</label>
-                </div>
-
-                <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-            </form>
-        </main>
-    );
+    //         <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+    //     </form>
+    // </main>
+  );
 };
 
 export default Login;
--- react-admin/src/pages/Users.tsx
+++ react-admin/src/pages/Users.tsx
@@ -1,72 +1,101 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import Layout from "../components/Layout";
 import axios from "axios";
-import {User} from "../models/user";
+import { User } from "../models/user";
 import {
-    Button,
-    Table,
-    TableBody,
-    TableCell,
-    TableFooter,
-    TableHead,
-    TablePagination,
-    TableRow
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TablePagination,
+  TableRow,
 } from "@material-ui/core";
 
 const Users = () => {
-    const [users, setUsers] = useState<User[]>([]);
-    const [page, setPage] = useState(0);
-    const perPage = 10;
+  const [users, setUsers] = useState<User[]>([]);
+  const [page, setPage] = useState(0);
 
-    useEffect(() => {
-        (
-            async () => {
-                const {data} = await axios.get('ambassadors');
+  const perPage = 10;
+  const [updated, setUpdated] = useState(false);
 
-                setUsers(data);
-            }
-        )()
-    }, []);
+  useEffect(() => {
+    (async () => {
+      await axios
+        .get("http://localhost:8081/api/users")
+        .then((res) => {
+          setUsers(res.data);
+        })
+        .catch(() => {
+          setUsers([]);
+        });
+    })();
+  }, [updated]);
 
-    return (
-        <Layout>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>#</TableCell>
-                        <TableCell>Name</TableCell>
-                        <TableCell>Email</TableCell>
-                        <TableCell>Actions</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {users.slice(page * perPage, (page + 1) * perPage).map(user => {
-                        return (
-                            <TableRow key={user.id}>
-                                <TableCell>{user.id}</TableCell>
-                                <TableCell>{user.first_name} {user.last_name}</TableCell>
-                                <TableCell>{user.email}</TableCell>
-                                <TableCell>
-                                    <Button variant="contained" color="primary"
-                                            href={`users/${user.id}/links`}
-                                    >View</Button>
-                                </TableCell>
-                            </TableRow>
-                        )
-                    })}
-                </TableBody>
-                <TableFooter>
-                    <TablePagination
-                        count={users.length}
-                        page={page}
-                        onPageChange={(e, newPage) => setPage(newPage)}
-                        rowsPerPage={perPage}
-                        rowsPerPageOptions={[]}
-                    />
-                </TableFooter>
-            </Table>
-        </Layout>
-    );
+  console.log(document.cookie);
+
+  return (
+    <Layout>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>#</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Email</TableCell>
+            <TableCell>Actions</TableCell>
+            <TableCell>Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {users.slice(page * perPage, (page + 1) * perPage).map((user) => {
+            return (
+              <TableRow key={user.id}>
+                <TableCell>{user.id}</TableCell>
+                <TableCell>
+                  {user.first_name} {user.last_name}
+                </TableCell>
+                <TableCell>{user.email}</TableCell>
+                <TableCell>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    href={`users/${user.id}/links`}
+                  >
+                    View
+                  </Button>
+                </TableCell>
+                <TableCell>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    onClick={async () => {
+                      await axios
+                        .delete(`http://localhost:8080/api/user/${user.id}`)
+                        .then(() => {
+                          setUpdated((prevState) => !prevState);
+                        });
+                    }}
+                  >
+                    Delete
+                  </Button>
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+        <TableFooter>
+          <TablePagination
+            count={users.length}
+            page={page}
+            onPageChange={(e, newPage) => setPage(newPage)}
+            rowsPerPage={perPage}
+            rowsPerPageOptions={[]}
+          />
+        </TableFooter>
+      </Table>
+    </Layout>
+  );
 };
 
 export default Users;
